ef1fb97cb8f8b57a18c4263ed966743bc132eb90
cesarzea1 <133278210+cesarzea1@users.noreply.github.com>
2023-09-03 13:31:43 -0400

    Project 2 Week 13 updates
    Project 2 Week 13 updates

---
+++ Select queries to check tables structure.sql
@@ -0,0 +1,12 @@
+-- Verify the table creation by running a SELECT statement for each table.
+-- verify 'campaign'
+SELECT * FROM campaign;
+
+-- verify 'category'
+SELECT * FROM category;
+
+-- verify 'contacts'
+SELECT * FROM contacts;
+
+-- verify 'subcategory'
+SELECT * FROM subcategory;
